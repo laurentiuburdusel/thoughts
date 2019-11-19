818c1b8c080b209d28d8e18dadef1619ec6460b5
Laurentiu <laurentiu.burdusel@edenred.com>
2019-11-19 23:35:15 +0200

    Updates

---
+++ precache-manifest.f39adadd603b6810dad8850b63fbebe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3339621037bdaec274edcae34767d4f9",
+    "url": "/thoughts/index.html"
+  },
+  {
+    "revision": "d8844f16f029068003b5",
+    "url": "/thoughts/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "e76210fd0f23d0caa13e",
+    "url": "/thoughts/static/js/2.e7d08b96.chunk.js"
+  },
+  {
+    "revision": "d8844f16f029068003b5",
+    "url": "/thoughts/static/js/main.f3a0a487.chunk.js"
+  },
+  {
+    "revision": "1154ec0d43a748e86533",
+    "url": "/thoughts/static/js/runtime-main.b3771fa7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/thoughts/static/media/logo.25bf045c.svg"
+  }
+]);
